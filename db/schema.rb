8a1eda7a907124d06df249b89c0dccaf57c02a57
Nathan Keller <keller.nathan@gmail.com>
2020-02-04 16:02:33 -0700

    Review class created. Following test errors. Need to estabilish relationship next

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200202222335) do
+ActiveRecord::Schema.define(version: 20200204225740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20200202222335) do
     t.string "description"
     t.string "adoption_status", default: "open"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "content"
+    t.string "picture"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shelters", force: :cascade do |t|
