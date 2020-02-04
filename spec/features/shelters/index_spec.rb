07b86a264f29ac7e6a9dfcc4c9fa1a3dd15984d3
Nathan Keller <keller.nathan@gmail.com>
2020-02-03 17:43:40 -0700

    Refactor shelers index test to include more tests

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,30 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
-  it "can see all the shelters avaiable" do
-    shelter_1 = Shelter.create(
+  describe "as a a visitor do"
+    before(:each) do
+      @shelter_1 = Shelter.create(
                          name: "Mike's Shelter",
                          address: "1331 17th Street",
                          city: "Denver",
                          state: "CO",
                          zip: 80202
                        )
-    shelter_2 = Shelter.create(
+      @shelter_2 = Shelter.create(
                           name: "Meg's Shelter",
                           address: "150 Main Street",
                           city: "Hershey",
                           state: "PA",
                           zip: 17033
                         )
+    end
 
-    visit "/shelters"
+    it "can see all the shelters avaiable" do
+      visit "/shelters"
 
-    expect(page).to have_content(shelter_1.name)
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+      expect(page).to have_link('Create New Shelter')
+      expect(page).to have_link('Edit Shelter')
+      expect(page).to have_button('Delete Shelter')
+  end
+
+  it 'has links to shelters' do
+    visit '/shelters'
+
     expect(page).to have_link("Mike's Shelter")
-    expect(page).to have_content(shelter_2.name)
+
+    click_link(@shelter_1.name)
+
+    expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+
+    visit '/shelters'
+
     expect(page).to have_link("Meg's Shelter")
-    expect(page).to have_link("Create New Shelter")
-    expect(page).to have_link("Edit Shelter")
-    expect(page).to have_button("Delete Shelter")
+
+    click_link(@shelter_2.name)
+
+    expect(current_path).to eq("/shelters/#{@shelter_2.id}")
   end
 end
