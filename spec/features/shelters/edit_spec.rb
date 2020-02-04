f0a3246608ad065930bdafa6c091d32f1e184187
Nathan Keller <keller.nathan@gmail.com>
2020-02-03 18:02:10 -0700

    Refactor shelters edit spec

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,26 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "shelters edit page", type: :feature do
-  it "can edit an individual shelter" do
-    shelter_1 = Shelter.create(
-                         name: "Mike's Shelter",
-                         address: "1331 17th Street",
-                         city: "Denver",
-                         state: "CO",
-                         zip: 80202
-                       )
+  describe "as a visitor" do
+      before(:each) do
+        @shelter_1 = Shelter.create(name: "Denver Shelter",
+                                    address: "123 fake st.",
+                                    city: "Denver",
+                                    state: "CO",
+                                    zip: "80018")
 
-    visit "/shelters/#{shelter_1.id}"
+        @shelter_2 = Shelter.create(name: "Aurora Shelter",
+                                    address: "800 Some Ave",
+                                    city: "Aurora",
+                                    state: "CO",
+                                    zip: "80249")
+      end
 
-    click_on "Update Shelter"
+    it "can edit an individual shelter" do
 
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+      visit "/shelters/#{@shelter_1.id}"
 
-    fill_in :name, with: "Nate's Shelter"
-    fill_in :address, with: "123 Bark Plaza"
-    fill_in :city, with: "Aurora"
-    fill_in :state, with: "CO"
-    fill_in :zip, with: 80014
-    click_on 'Edit Shelter'
+      expect(page).to have_link('Update Shelter')
+
+      click_on "Update Shelter"
+
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}/edit")
+
+      fill_in :name, with: "Nate's Shelter"
+      fill_in :address, with: "123 Bark Plaza"
+      fill_in :city, with: "Aurora"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: 80014
+
+      click_on 'Edit Shelter'
+
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+
+      expect(page).to have_content("Nate's Shelter")
+      expect(page).to_not have_content(@shelter_1.name)
+    end
   end
 end
