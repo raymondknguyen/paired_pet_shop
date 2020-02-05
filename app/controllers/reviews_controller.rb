2507ba1c3947e35dfd6fedc437b4f49eba1e0baa
Nathan Keller <keller.nathan@gmail.com>
2020-02-05 11:52:50 -0700

    Update action in review controller complete.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,19 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
+  def update
+    shelter = Shelter.find(params[:shelter_id])
+    review = Review.find(params[:id])
+    review.update(review_params)
+    if review.save
+      flash[:success] = "Thanks for updating your review"
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:error] = "#{shelter.name} review not updated. Missing Field"
+      redirect_to "/shelters/#{shelter.id}/reviews/new"
+    end
+  end
+
   private
   def review_params
     params.permit(:title, :rating, :content, :picture)
