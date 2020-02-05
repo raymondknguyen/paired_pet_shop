4c884db0f315d4c21ff4e00da180b4d583d0fb52
Nathan Keller <keller.nathan@gmail.com>
2020-02-05 14:54:40 -0700

    Create test for favorites indicator
    Co-Authored-By: Ray <49219371+itemniner@users.noreply.github.com>

--- spec/features/favorites/indicator_spec.rb
+++ spec/features/favorites/indicator_spec.rb
@@ -1,1 +1,11 @@
-require 'rails_helpter'
+require 'rails_helper'
+
+
+RSpec.describe 'favorites indicator in navigation bar' do
+  it 'shows a count of favorited pets'do
+
+    visit "/shelters"
+
+    expect(page).to have_content('(0) Favorited Pets')
+  end
+end
