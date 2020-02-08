8a3c38c7922bd080319bcdb6daca6a1987d787f2
Nathan Keller <keller.nathan@gmail.com>
2020-02-08 15:52:04 -0700

    Refactor application create method with a helper method.

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,17 +8,20 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def collect_pets
+    pets = Pet.all
+    @all_favs = pets.find_all do |pet|
+      session[:favorites].find do |favorite|
+        pet.id == favorite
+      end
+    end
+  end
+
   def create
-    @application = Application.create(application_params)
-    if @application.save
+    application = Application.create(application_params)
+    if application.save
       flash[:success] = "Application Submitted Successfully"
-      @application.pets = (
-      pets = Pet.all
-      @all_favs = pets.find_all do |pet|
-        session[:favorites].find do |favorite|
-          pet.id == favorite
-        end
-      end)
+      application.pets = collect_pets
       session[:favorites] = []
       redirect_to '/favorites'
     else
