baaf60ae52d10c44c517491fd091ecbe05009be2
Nathan Keller <keller.nathan@gmail.com>
2020-02-10 21:35:12 -0700

    Create Test for shelter_pets new

--- spec/features/shelter_pets/new_spec.rb
+++ spec/features/shelter_pets/new_spec.rb
@@ -36,5 +36,30 @@ RSpec.describe 'create pet page', type: :feature do
       expect(page).to have_content(new_pet.age)
       expect(page).to have_content(new_pet.sex)
     end
+    it "has a flash message if a form is incomplete when creating a pet" do
+      shelter_1 = Shelter.create(
+                           name: "Mike's Shelter",
+                           address: "1331 17th Street",
+                           city: "Denver",
+                           state: "CO",
+                           zip: 80202
+                         )
+
+      visit "/shelters/#{shelter_1.id}/pets"
+
+      click_link 'Add New Pet'
+
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/pets/new")
+
+      #No name included in form
+      fill_in :image, with: "https://image.shutterstock.com/image-photo/dog-260nw-587562362.jpg"
+      fill_in :description, with: "Cute"
+      fill_in :age, with: 0
+      fill_in :sex, with: "Female"
+
+      click_on 'Create Pet'
+
+      expect(page).to have_content("Error, Missing Fields. Name")
+    end
   end
 end
