e739154689c84cc0834a0f4fb93fc64d9fbc4671
Nathan Keller <keller.nathan@gmail.com>
2020-02-11 10:59:11 -0700

    create logic for flash message in shelters update action

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -27,7 +27,12 @@ class SheltersController < ApplicationController
     shelter = Shelter.find(params[:id])
     shelter.update(shelter_params)
 
-    redirect_to "/shelters/#{shelter.id}"
+    if shelter.save
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:error] = "#{shelter.errors.full_messages.to_sentence}"
+      redirect_to "/shelters/#{shelter.id}/edit"
+    end
   end
 
   def destroy
@@ -35,10 +40,11 @@ class SheltersController < ApplicationController
     if shelter.pets.any? do |pet|
       pet.adoption_status = "pending"
     end
-    flash[:error] = "Can not delete shelter with pending pet application"
-  else
-    shelter.destroy
-  end
+      flash[:error] = "Can not delete shelter with pending pet application"
+    else
+      shelter.destroy
+    end
+
     redirect_to '/shelters'
   end
 
