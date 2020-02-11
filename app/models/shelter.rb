e239f3c2a2da2754d7a4a1801bb4bd7d859456e7
Nathan Keller <keller.nathan@gmail.com>
2020-02-11 12:02:00 -0700

    Model logic for shelter pet count passing tests.

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,9 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def pet_count
+    pets.count
+  end
+
 end
