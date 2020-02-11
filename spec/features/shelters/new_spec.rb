c5eb2f5b385e2c219303ed8e6fe670a8d0f71dad
Nathan Keller <keller.nathan@gmail.com>
2020-02-11 11:05:32 -0700

    Create test to display flash message if shelter form is missing fields

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -29,4 +29,25 @@ RSpec.describe "shelter new page", type: :feature do
     new_shelter = Shelter.last
     expect(page).to have_content("Nate's Shelter")
   end
+
+  it "shows an error message when a form is blank" do
+
+    visit "/shelters"
+    expect(page).to have_link("Create New Shelter")
+
+    click_on "Create New Shelter"
+    expect(current_path).to eq('/shelters/new')
+
+    #name is blank
+    fill_in :name, with: ""
+    fill_in :address, with: "123 Bark Plaza"
+    fill_in :city, with: "Aurora"
+    fill_in :state, with: "CO"
+    fill_in :zip, with: 80014
+
+    click_on "Create Shelter"
+
+    expect(page).to have_content("Name can't be blank")
+  end
+
 end
